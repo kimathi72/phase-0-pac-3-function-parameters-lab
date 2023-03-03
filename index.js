7aae45f08e78fece20d160c0b769a9bf0dee5654
roy <kimathiwaweru@gmail.com>
2023-03-03 18:31:48 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
+//takes in an argument of a name and returns a phrase with that name using string interpolation
+
 function introduction(name){
         return `Hi, my name is ${name}.`;
     
 }
+//takes in two arguments, a name and a language, and returns a phrase using those arguments
 function introductionWithLanguage(name, language){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
-
+//takes in two arguments, a name and a language, and the default value can be overridden with an argument
 function introductionWithLanguageOptional(name, language = "Javascript"){
     return `Hi, my name is ${name} and I am learning to program in ${language}.`;
 }
